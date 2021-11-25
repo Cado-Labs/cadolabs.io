0c6ca2bb65d99fa23ee6bc7eb35b5ca1ad728cf2
kei4i <ilovegirlsinaglasses@gmail.com>
2021-11-26 02:30:39 +0300

    - edit header menu - add  viewport - edit careers page - edit styles - add js menu toggler - add new image

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,13 @@
-console.log('hello from webpack');
+const toggleMenu = document.querySelector('.menu-toggler');
+const headerMenu = document.querySelector('header nav.menu');
+
+toggleMenu.addEventListener('click', event => {
+    headerMenu.classList.toggle('active');
+});
+
+document.addEventListener('click', event => {
+   if (event.target.closest('nav.menu')) {
+       return;
+   }
+    headerMenu.classList.remove('active');
+});
