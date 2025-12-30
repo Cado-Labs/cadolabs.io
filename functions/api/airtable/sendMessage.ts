afd8bd5874ca7a3544ecd226cde7e506f8c692ab
denis.kha <denis.kha@cadolabs.io>
2025-12-30 06:15:10 -0300

    edited sendMessage

--- functions/api/airtable/sendMessage.ts
+++ functions/api/airtable/sendMessage.ts
@@ -1,31 +1,49 @@
-export async function onRequestPost(context) {
+import getAccessToken from "../../utils/getAccessToken";
+
+export async function onRequestPost(context: any) {
     const {
-        env, // same as existing Worker API
+        env,
         request
     } = context;
+
     const bodyData = await request.formData();
     const message = {
-      name: bodyData.get('name') as string,
-      message: bodyData.get('message') as string,
-      email: bodyData.get('email') as string,
-    }
+        name: bodyData.get('name') as string,
+        email: bodyData.get('email') as string,
+        message: bodyData.get('message') as string,
+        date: new Date().toISOString(),
+    };
 
-    return await fetch(`https://api.airtable.com/v0/${env.BASE_ID}/${env.FEEDBACK_TABLE}`, {
+    const token = await getAccessToken(env);
+    const url = `https://sheets.googleapis.com/v4/spreadsheets/${env.GSHEET_VACANCIES_ID}/values/Feedbacks!A1:append?valueInputOption=USER_ENTERED`;
+
+    const response = await fetch(url, {
         method: 'POST',
-        body: JSON.stringify({
-            "records": [
-                {
-                    "fields": {
-                        "name": message.name,
-                        "email": message.email,
-                        "message": message.message,
-                    }
-                },
-            ]
-        }),
         headers: {
-            Authorization: `Bearer ${env.AIRTABLE_API_KEY}`,
+            Authorization: `Bearer ${token}`,
             'Content-Type': 'application/json',
         },
+        body: JSON.stringify({
+            majorDimension: "ROWS",
+            values: [
+                [
+                    message.date,
+                    message.name || '',
+                    message.email || '',
+                    message.message || ''
+                ]
+            ],
+        }),
     });
+
+    if (!response.ok) {
+        const errorDetail = await response.json() as any;
+
+        return new Response(JSON.stringify(errorDetail), {
+            status: response.status,
+            headers: { 'Content-Type': 'application/json' }
+        });
+    }
+
+    return response;
 }
